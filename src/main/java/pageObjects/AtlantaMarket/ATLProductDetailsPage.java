8fe34a76f6e4b461ed493821630c4709ce80cadb
Nilam Mahajan <75731650+nilamma21@users.noreply.github.com>
2022-06-21 11:19:41 +0530

    Added TCs of Product actions

--- src/main/java/pageObjects/AtlantaMarket/ATLProductDetailsPage.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLProductDetailsPage.java
@@ -1,5 +1,7 @@
 package pageObjects.AtlantaMarket;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,6 +24,23 @@ public class ATLProductDetailsPage {
 	By allListNames = By.xpath("//li[@class='imc-market-planner-list-draggable-item']"); //Locator for All List Names
 	By addToSelectedBtn = By.xpath("//div[@class='imc-button--justify-right']/input[1]"); //Locator for Add to Selected Btn
 	By productNameFromList = By.xpath("(//div[@class='imc-saved-exhibitors__contentItems--item'])[1]/div[1]/div[1]/a[1]"); //Locator for product name from list
+	By productaddtofavicon = By.xpath("//label[@aria-label='Favorite']"); //Locator for Add To Favorite icon for Product on details page
+
+	By productAddNote = By.xpath("//div[@class='imc-addnote imc-expand-collapse']/div/button/label[1]"); //Locator for Add Note icon for Product on details page
+	By addNotePopup = By.xpath("//div[@class='imc-addnote imc-expand-collapse']/div/button/label[1]"); //Locator for Add Note Popup 
+	By addNoteTitleTextbox = By.xpath("(//div[@class='imc-formfield imc-content imc-addnote-modal__text-area imc-type--title-5-link'])[1]/input[1]"); //Locator for Add Note Text box
+	By addNoteContainsTextArea= By.xpath("(//div[@class='imc-formfield imc-content imc-addnote-modal__text-area imc-type--title-5-link'])[2]/textarea[1]"); //Locator for Add Note Contains
+	By addNoteSaveBtn= By.xpath("//div[@class='imc-addnote-modal__actions-wrapper']/input[@type='submit']"); //Locator for Save btn
+	By viewAllNotes= By.xpath("//div[@class='imc-addnote-modal__actions-wrapper']/a[1]"); //Locator for View All Notes
+	By viewAllNotesPopupHeader= By.xpath("//div[@class='ReactModal__Content ReactModal__Content--after-open imc-modal--box imc-addnote-modal']/div[1]/div[1]/div[1]/h4[1]"); //Locator for View All Notes Header
+	By allNotesList= By.xpath("//ul[@class='imc-market-planner-list imc-addnote-modal__list']/li"); //Locator for All Notes
+	By verifyNoteTitle= By.xpath("//div[@class='imc-addnote-modal__padded-top-bottom']/h5[1]"); //Locator for note title
+
+	By productfullscreenviewerbtn = By.xpath("//div[@class='imc-product-details--gallery--image-fullscreen']"); //Locator for Product Full Screen Viewer btn
+	By productfullscreenviewer = By.xpath("//div[@class='imc-modal--gallery--section']"); //Locator for Product Full Screen Viewer
+	By prodfullscreenviewertitle = By.xpath("//div[@class='imc-modal--gallery--section']/h2"); //Locator for Product Full Screen Viewer title
+	
+
 	
 	public ATLProductDetailsPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
@@ -91,6 +110,74 @@ public class ATLProductDetailsPage {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(productNameFromList));
 		return driver.findElement(productNameFromList);
 	}
+	public WebElement getProductAddToFavIcon() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productaddtofavicon));
+		return driver.findElement(productaddtofavicon);
+	}
+
+	public WebElement getProductAddNoteIcon() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productAddNote));
+		return driver.findElement(productAddNote);
+	}
+	public WebElement getAddNotePopup() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(addNotePopup));
+		return driver.findElement(addNotePopup);
+	}
+	public WebElement getAddNoteTitleTextbox() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(addNoteTitleTextbox));
+		return driver.findElement(addNoteTitleTextbox);
+	}
+	public WebElement getAddNoteContainsTextArea() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(addNoteContainsTextArea));
+		return driver.findElement(addNoteContainsTextArea);
+	}
+	public WebElement getAddNoteSaveBtn() {
+		wait = new WebDriverWait (driver,20);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated(addNoteSaveBtn));
+		wait.until(ExpectedConditions.elementToBeClickable(addNoteSaveBtn));
+		return driver.findElement(addNoteSaveBtn);
+	}
+	public WebElement getViewAllNotes() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(viewAllNotes));
+		return driver.findElement(viewAllNotes);
+	}
+	public WebElement getViewAllNotesPopupHeader() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(viewAllNotesPopupHeader));
+		return driver.findElement(viewAllNotesPopupHeader);
+	}
+	
+	public List <WebElement> getAllNotesList() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(allNotesList));
+		return driver.findElements(allNotesList);
+	}
+	public WebElement getVerifyNoteTitle() {
+			wait = new WebDriverWait (driver,20);
+			wait.until(ExpectedConditions.visibilityOfElementLocated(verifyNoteTitle));
+			return driver.findElement(verifyNoteTitle);
+		}
+
+	public WebElement getProductFullScreenViewerBtn() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productfullscreenviewerbtn));
+		return driver.findElement(productfullscreenviewerbtn);
+	}
+	public WebElement getProductFullScreenViewer() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productfullscreenviewer));
+		return driver.findElement(productfullscreenviewer);
+	}
+	public WebElement getProductFullScreenViewerTitle() {
+		return driver.findElement(prodfullscreenviewertitle);
+	}
+
 }
 
 
