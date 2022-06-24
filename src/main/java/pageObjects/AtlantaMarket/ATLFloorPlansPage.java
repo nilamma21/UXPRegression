9c2be4bf873464864075b2174d481c221c91aa38
Nilam Mahajan <75731650+nilamma21@users.noreply.github.com>
2022-06-24 10:42:37 +0530

    Updated FloorPlan_Icons TC

--- src/main/java/pageObjects/AtlantaMarket/ATLFloorPlansPage.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLFloorPlansPage.java
@@ -15,18 +15,27 @@ public class ATLFloorPlansPage {
 	By atlFloorPlansLink = By.xpath("//div[@class='imc-section']/div[1]/div[2]/div[2]/div[1]/div[2]"); //Locator for floor plans Link
 	By atlBuildingFloor = By.xpath("//div[@class='imc-campus-view']/div[3]/div[12]/div[3]/a[1]"); //Locator for Building floor
 	By atlBuildingFloorNumber = By.xpath("//div[@class='imc-campus-view']/div[1]/div[23]/div[2]"); //Locator for Building floor number
-	//By atlFloorName = By.xpath("//div[@class='imc-floor-plan']/div[1]/div[1]/span[1]/div[2]"); //Locator for floor name
 	By atlFloorName = By.xpath("//div[@class='imc-type--title-6']");
-	By atlNoExpMsg = By.xpath("(//div[@class='section-wrapper imc-section--'])[1]/section[1]/div[1]/div[1]/div[1]/div[3]/p[2]"); //Locator for No exhibitor msg
-	By atlLoadingExhMsg = By.xpath("(//div[@class='section-wrapper imc-section--'])[1]/section[1]/div[1]/div[1]/div[1]/div[3]/p[]"); //Locator for Exhibitor loading msg
-	By atlNoExhibitorFloor = By.xpath("//div[@class='imc-campus-view']/div[1]/div[18]/div[3]/a[1]"); //Locator for No Exhibitor
+	By atlnoexhibitorsmsg = By.xpath("//p[contains(text(),'No Exhibitors on This Floor')]"); //Locator for No exhibitor msg
+	By atlloadingexhmsg = By.xpath("//p[contains(text(),'Loading Exhibitors Please Wait ...')]"); //Locator for Loading Exhibitors msg
+	By atlNoExhibitorFloor = By.xpath("//a[contains(text(),'Buying Groups / Meeting Space')]"); //Locator for No Exhibitor
 	By atlExhibitorFloorZoomIn = By.xpath("//div[@class='meridian-zoom-controls meridian--private--1qg3nfg']/button[1]"); //Locator for Zoom In Exhibitor
 	By atlExhibitorFloorZoomOut = By.xpath("//div[@class='meridian-zoom-controls meridian--private--1qg3nfg']/button[2]"); //Locator for Zoom In Exhibitor
-	By atlZoomInOutAttribute = By.xpath("//div[@class='imc-map-container']/div[1]/div[1]/div[5]/div[1]"); //Locator for Zoom In out Attribute
-	By atlIcon = By.xpath("//div[@class='imc-map-container']/div[1]/div[1]/div[5]/div[1]/div[1]/div[19]"); //Locator for Icon
-	By atlIconText = By.xpath("//div[@class='imc-map-container']/div[1]/div[1]/div[5]/div[1]/div[1]/div[19]/div[1]"); //Locator for Icon text
-	By atlIconPopupText = By.xpath("//div[@class='imc-map-container']/div[1]/div[1]/div[3]/div[2]/p[1]"); //Locator for Icon popup Text
+	By atlfloorplanmapimage = By.xpath("//div[@class='imc-map-container']/div[1]/div[1]/div[5]/div[1]"); //Locator for Zoom In out Attribute
 	By atlSelectBox = By.xpath("//select[@class='imc-searchform--dropdown imc-floor-plan--dropdown']"); //Locator Selectbox
+	By vendingmchineicononmap = By.xpath("//button[contains(@class, 'vending_machines')]"); //Locator for Vending Machine icon on Map image
+	By vendingmachineoverlay = By.xpath("//div[contains(@style, 'vending_machines')]"); //Locator for Vending Machine overlay on Map
+	By elevatoricononmap = By.xpath("(//button[contains(@class, 'elevator')])[position()=1]"); //Locator for elevator icon on Map image
+	By elevatoroverlay = By.xpath("//div[contains(@style, 'elevator')]"); //Locator for elevator overlay on Map
+	By waterfountainicononmap = By.xpath("(//button[contains(@class, 'water_fountain')])[position()=3]"); //Locator for Water Fountain icon on Map image
+	By waterfountainoverlay = By.xpath("//div[contains(@style, 'water_fountain')]"); //Locator for Water Fountain overlay on Map
+	By phoneicononmap = By.xpath("(//button[contains(@class, 'phone')])[position()=3]"); //Locator for Phone icon on Map image
+	By phoneoverlay = By.xpath("//div[contains(@style, 'phone')]"); //Locator for Phone overlay on Map
+	//By locationpinicononmap = By.xpath("(//div[@class='meridian-label meridian--private--nfhgl4-placemark-label'])[position()=10]"); //Locator for Location pin of any Exhibitor on Map
+	By locationpinicononmap = By.xpath("(//button[contains(@class, 'generic')])[position()=9]");
+	By exhibitordetailsmodal = By.xpath("//div[@class='imc-content--padded-medium-desktop']"); //Locator for Exhibitor Details Modal
+	By exhnameonexhdetailsmodal = By.xpath("//div[@class='imc-content--padded-medium-desktop']/div/div/h5"); //Locator for Exhibitor name on Exh Details Modal
+	By overlayclosebtn = By.xpath("//button[@data-testid='meridian--private--close-overlay']"); //Locator for Overlay close btn
 	
 	public ATLFloorPlansPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
@@ -60,13 +69,8 @@ public class ATLFloorPlansPage {
 	}
 	public WebElement getATLNoExpMsg() {
 		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlNoExpMsg));
-		return driver.findElement(atlNoExpMsg);
-	}
-	public WebElement getATLLoadingExhMsg() {
-		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlLoadingExhMsg));
-		return driver.findElement(atlLoadingExhMsg);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(atlnoexhibitorsmsg));
+		return driver.findElement(atlnoexhibitorsmsg);
 	}
 	public WebElement getATLNoExhibitorFloor() {
 		wait = new WebDriverWait (driver,20);
@@ -84,32 +88,80 @@ public class ATLFloorPlansPage {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(atlExhibitorFloorZoomOut));
 		return driver.findElement(atlExhibitorFloorZoomOut);
 	}
-	public WebElement getATLZoomInOutAttribute() {
+	public WebElement getATLFloorPlanMapIamge() {
 		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlZoomInOutAttribute));
-		return driver.findElement(atlZoomInOutAttribute);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(atlfloorplanmapimage));
+		return driver.findElement(atlfloorplanmapimage);
 	}
-	public WebElement getATLIcon() {
-		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlIcon));
-		return driver.findElement(atlIcon);
-	}
-	public WebElement getATLIconText() {
-		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlIconText));
-		return driver.findElement(atlIconText);
-	}
-	public WebElement getATLIconPopupText() {
-		wait = new WebDriverWait (driver,20);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(atlIconPopupText));
-		return driver.findElement(atlIconPopupText);
-	}
+
 	public WebElement getATLSelectBox() {
 		wait = new WebDriverWait (driver,20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(atlSelectBox));
 		return driver.findElement(atlSelectBox);
 	}
-
+	
+	public WebElement getATLLoadingExhMsg() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(atlloadingexhmsg));
+		return driver.findElement(atlloadingexhmsg);
+	}
+	public WebElement getVendingMachineIconOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(vendingmchineicononmap));
+		return driver.findElement(vendingmchineicononmap);
+	}
+	public WebElement getVendingMachineOverlayOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(vendingmachineoverlay));
+		return driver.findElement(vendingmachineoverlay);
+	}
+	public WebElement getElevatorIconOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.elementToBeClickable(elevatoricononmap));
+		return driver.findElement(elevatoricononmap);
+	}
+	public WebElement getElevatorOverlayOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(elevatoroverlay));
+		return driver.findElement(elevatoroverlay);
+	}
+	public WebElement getWaterFountainIconOnMap() throws InterruptedException {
+		//Thread.sleep(4000);
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.elementToBeClickable(waterfountainicononmap));
+		return driver.findElement(waterfountainicononmap);
+	}
+	public WebElement getWaterFountainOverlayOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(waterfountainoverlay));
+		return driver.findElement(waterfountainoverlay);
+	}
+	public WebElement getPhoneIconOnMap() throws InterruptedException{
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.elementToBeClickable(phoneicononmap));
+		return driver.findElement(phoneicononmap);
+	}
+	public WebElement getPhoneOverlayOnMap() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(phoneoverlay));
+		return driver.findElement(phoneoverlay);
+	}
+	public WebElement getLocationPinIconOnMap() throws InterruptedException{
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.elementToBeClickable(locationpinicononmap));
+		return driver.findElement(locationpinicononmap);
+	}
+	public WebElement getExhibitorDetailsModal() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(exhibitordetailsmodal));
+		return driver.findElement(exhibitordetailsmodal);
+	}
+	public WebElement getExhNameOnExhibitorDetailsModal() {
+		return driver.findElement(exhnameonexhdetailsmodal);
+	}
+	public WebElement getOverlayCloseBtn() {
+		return driver.findElement(overlayclosebtn);
+	}
 }
 
 
--- src/test/java/atlantamarket/FloorPlans.java
+++ src/test/java/atlantamarket/FloorPlans.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -59,13 +60,13 @@ public class FloorPlans extends base {
 		Thread.sleep(10000);
 		lap.getCloseMarktAdBtn().click();
 
-		// Login to Market Planner
+		/*	// Login to Market Planner
 		utl.verifyMPLoginFunctionality();
 
-		Thread.sleep(6000);
-		lap.getCloseMarktAdBtn().click();
+		Thread.sleep(8000);
+		lap.getCloseMarktAdBtn().click();*/
 	}
-	
+
 	@Test(priority = 1)
 	public void TS001_VerifyNavigationToDifferentFloorBuildingsTest() throws InterruptedException, IOException {
 		// The purpose of this test case to verify:-
@@ -79,28 +80,36 @@ public class FloorPlans extends base {
 
 		// Click on Exh And Product Tab
 		atlflpp.getATLExhibitorsAndProductTab().click();
-		
+
 		//click on Floor plans link
 		atlflpp.getATLFloorPlansLink().click();
-
 		Thread.sleep(5000);
+
 		//Verify that user should redirect to Floor plans page
 		Assert.assertTrue(driver.getCurrentUrl().contains(prop.getProperty("atlmrkturl_uat")+"Market-Map"));
-		
-		//Click on Building floor
+
+		//Click on Building/floor
 		String floorName=atlflpp.getATLBuildingFloor().getText();
 		System.out.println("Floor Name : " +floorName);
+
+		String locationlink = atlflpp.getATLBuildingFloor().getAttribute("href");
+		System.out.println(locationlink);
+
 		atlflpp.getATLBuildingFloor().click();
-		
-		//Verify floor name
+		Thread.sleep(5000);
+
+		// Verify that selected building-floor plan page should be opened
+		Assert.assertTrue(locationlink.equals(driver.getCurrentUrl()));
+
+		//Verify selected floor name
 		Assert.assertTrue(atlflpp.getATLFloorName().getText().contains(floorName));
 	}
 
-	
 	@Test(priority = 3)
-	public void TS003_VerifyFloorPlansNoExhibitorsLoadingMessageTest() throws InterruptedException, IOException {
+	public void TS003_VerifyNoExhibitorsOnThisFloorMessageTest() throws InterruptedException, IOException {
 		// The purpose of this test case to verify:-
 		// UXP-T286: To verify Floor Plans: No Exhibitors or Loading message
+
 		lap = new ATLLandingPage(driver);
 		lp = new ATLLoginPage(driver);
 		utl = new Utility(driver);
@@ -110,27 +119,25 @@ public class FloorPlans extends base {
 
 		// Click on Exh And Product Tab
 		atlflpp.getATLExhibitorsAndProductTab().click();
+
 		//click on Floor plans link
 		atlflpp.getATLFloorPlansLink().click();
-		String floorPlansPageUrl=prop.getProperty("floorPlansPageURL");
-		driver.getCurrentUrl().contains(floorPlansPageUrl);
-		//Verify Floor plans page URL
-		Assert.assertTrue(driver.getCurrentUrl().contains(floorPlansPageUrl));
-		//Click on Building floor
-		String floorName=atlflpp.getATLBuildingFloor().getText();
-		System.out.println("Floor Name : " +floorName);
-		//click on No Exhibitor floor
+
+		//click on Floor whose not having an Exhibitors
 		atlflpp.getATLNoExhibitorFloor().click();
-		String noExhMsg=atlflpp.getATLNoExpMsg().getText();
-		System.out.println(noExhMsg);
-		//Verify No Exhibitor msg
-		Assert.assertTrue(atlflpp.getATLNoExpMsg().getText().contains(noExhMsg));
+
+		//Verify that Loading Exhibitors msg should be displayed
+		Assert.assertTrue(atlflpp.getATLLoadingExhMsg().isDisplayed());
+
+		//Verify that No Exhibitor msg should be displayed
+		Assert.assertTrue(atlflpp.getATLNoExpMsg().isDisplayed());
 	}
-	
+
 	@Test(priority = 4)
-	public void TS004_VerifyFloorPlansZoomInOutLevelTest() throws InterruptedException, IOException {
-		// The purpose of this test case to verify:-
+	public void TS004_VerifyZoomInOutLevelOnFloorPlansPageTest() throws InterruptedException, IOException {
+		// The purpose of this test case:-
 		// UXP-T287: To verify Floor Plans: Zoom Levels
+
 		lap = new ATLLandingPage(driver);
 		lp = new ATLLoginPage(driver);
 		utl = new Utility(driver);
@@ -140,46 +147,45 @@ public class FloorPlans extends base {
 
 		// Click on Exh And Product Tab
 		atlflpp.getATLExhibitorsAndProductTab().click();
+
 		//click on Floor plans link
 		atlflpp.getATLFloorPlansLink().click();
-		String floorPlansPageUrl=prop.getProperty("floorPlansPageURL");
-		driver.getCurrentUrl().contains(floorPlansPageUrl);
-		//Verify Floor plans page URL
-		Assert.assertTrue(driver.getCurrentUrl().contains(floorPlansPageUrl));
-		//Click on Building floor
-		String floorName=atlflpp.getATLBuildingFloor().getText();
-		System.out.println("Floor Name : " +floorName);
-		//click on No Exhibitor floor
-		atlflpp.getATLNoExhibitorFloor().click();
-		
+
+		//click on any floor
+		atlflpp.getATLBuildingFloor().click();
+
+		//Click on Zoom In icon
 		atlflpp.getATLExhibitorFloorZoomIn().click();
 		//Stored Zoom in Attribute
-		String x1=atlflpp.getATLZoomInOutAttribute().getAttribute("style");
+		String x1=atlflpp.getATLFloorPlanMapIamge().getAttribute("style");
+
 		atlflpp.getATLExhibitorFloorZoomIn().click();
 		//Stored Zoom in Attribute
-		String x2=atlflpp.getATLZoomInOutAttribute().getAttribute("style");
+		String x2=atlflpp.getATLFloorPlanMapIamge().getAttribute("style");
+
 		//Stored Zoom in Attribute
 		atlflpp.getATLExhibitorFloorZoomIn().click();
-		String x3=atlflpp.getATLZoomInOutAttribute().getAttribute("style");
-		
-		//Verify ZoomIn Attributes
+		String x3=atlflpp.getATLFloorPlanMapIamge().getAttribute("style");
+
+		//Verify Zoom In functionality
 		Assert.assertNotEquals(x2, x3);
-		
 		Thread.sleep(5000);
-		//zoom Out
+
+		//Click on Zoom Out icon
 		atlflpp.getATLExhibitorFloorZoomOut().click();
 		atlflpp.getATLExhibitorFloorZoomOut().click();
 		//Stored Zoom out Attribute
-		String out=atlflpp.getATLZoomInOutAttribute().getAttribute("style");
-		//Verify ZoomOut
+		String out=atlflpp.getATLFloorPlanMapIamge().getAttribute("style");
+
+		//Verify Zoom Out functionality
 		Assert.assertNotEquals(x1, out);
-		
 	}
 
 	@Test(priority = 5)
 	public void TS005_VerifyFloorPlansIconsTest() throws InterruptedException, IOException {
-		// The purpose of this test case to verify:-
+		// The purpose of this test case to:-
 		// UXP-T289: To verify Floor Plans: Icons
+
 		lap = new ATLLandingPage(driver);
 		lp = new ATLLoginPage(driver);
 		utl = new Utility(driver);
@@ -189,37 +195,63 @@ public class FloorPlans extends base {
 
 		// Click on Exh And Product Tab
 		atlflpp.getATLExhibitorsAndProductTab().click();
+
 		//click on Floor plans link
 		atlflpp.getATLFloorPlansLink().click();
-		String floorPlansPageUrl=prop.getProperty("floorPlansPageURL");
-		driver.getCurrentUrl().contains(floorPlansPageUrl);
-		//Verify Floor plans page URL
-		Assert.assertTrue(driver.getCurrentUrl().contains(floorPlansPageUrl));
-		//Click on Building floor
-		String floorName=atlflpp.getATLBuildingFloor().getText();
-		System.out.println("Floor Name : " +floorName);
-		//click on No Exhibitor floor
-		atlflpp.getATLNoExhibitorFloor().click();
-		//Click on ZoomIn +button
-		atlflpp.getATLExhibitorFloorZoomIn().click();
-		atlflpp.getATLExhibitorFloorZoomIn().click();
-		String atlIconHeader=atlflpp.getATLIconText().getText();
-		System.out.println("Icon Header : "+atlIconHeader);
-		//Click on Icon
-		atlflpp.getATLIcon().click();
+
+		//click on any floor
+		atlflpp.getATLBuildingFloor().click();
+
+		//Click on Vending Machine icon on Map image
+		atlflpp.getVendingMachineIconOnMap().click();
+
+		//Verify that Vending Machine Overlay should appeared on Map
+		Assert.assertTrue(atlflpp.getVendingMachineOverlayOnMap().isDisplayed());
+
+		//Click on Elevator icon on Map image
+		atlflpp.getElevatorIconOnMap().click();
+
+		//Verify that Elevator Overlay should appeared on Map
+		Assert.assertTrue(atlflpp.getElevatorOverlayOnMap().isDisplayed());
+
+		//Click on Water Fountain icon on Map image
+		atlflpp.getWaterFountainIconOnMap().click();
+
+		//Verify that Water Fountain Overlay should appeared on Map
+		Assert.assertTrue(atlflpp.getWaterFountainOverlayOnMap().isDisplayed());
+
+		//Click on Phone icon on Map image
+		atlflpp.getPhoneIconOnMap().click();
+
+		//Verify that Phone Overlay should appeared on Map
+		Assert.assertTrue(atlflpp.getPhoneOverlayOnMap().isDisplayed());
 		
-		//Verify Icon popup
-		System.out.println("Popup Text : "+atlflpp.getATLIconPopupText().getText());
-		Assert.assertTrue(atlflpp.getATLIconPopupText().getText().contains(atlIconHeader));
+		//Click on Overlay Close btn
+		atlflpp.getOverlayCloseBtn().click();
+		
+		
+		//Not working below code
+		/*//Click on Location pin of any Exhibitor on Map
+		Actions act = new Actions(driver);
+		act.doubleClick(atlflpp.getLocationPinIconOnMap()).perform();
+		act.click(atlflpp.getLocationPinIconOnMap()).perform();
+		
+		//Verify that select Exhibitor's location details should be displayed in popup
+		Assert.assertTrue(atlflpp.getExhibitorDetailsModal().isDisplayed());
+		
+		//Verify the Exhibitor name on Details modal
+		String exhnameonlocationpin = atlflpp.getLocationPinIconOnMap().getText();
+		Assert.assertEquals(atlflpp.getExhNameOnExhibitorDetailsModal().getText(), exhnameonlocationpin);*/
+
 	}
-	
-	
 
 
 
-	@AfterClass
+
+
+	/*	@AfterClass
 	public void tearDown()
 	{
 		//driver.quit();
-	}
+	}*/
 }
