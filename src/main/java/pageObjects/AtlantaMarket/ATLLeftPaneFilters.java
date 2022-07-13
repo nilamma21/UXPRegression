a9c2bbb146ff8cef880917b6834a357899e81170
SwapnilIrnak91 <swapnili@cybage.com>
2022-07-13 12:40:01 +0530

    Added Left Panel Filter Test

--- src/main/java/pageObjects/AtlantaMarket/ATLLeftPaneFilters.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLLeftPaneFilters.java
@@ -11,10 +11,12 @@ public class ATLLeftPaneFilters {
 	public WebDriver driver;
 	public WebDriverWait wait;
 
-	By prodcatgexpandbtn = By.xpath("//a[@aria-label='Product Categories']");
-	By accentfurnexpandbtn = By.xpath("//div[contains(@class,'imc-filter imc-content imc-expand-collapse')]//div[1]//div[1]//div[2]//div[1]//div[1]//div[1]//div[1]//a[1]//div[1]//div[1]//div[1]");
+	By prodcatgexpandbtn = By.xpath("//a[@aria-label='Product Categories']"); //Locator for Poduct category
+	By accentfurnexpandbtn = By.xpath("//div[contains(@class,'imc-filter imc-content imc-expand-collapse')]//div[1]//div[1]//div[2]//div[1]//div[1]//div[1]//div[1]//a[1]//div[1]//div[1]//div[1]");//Locator for accent furniture expan
+	By atlAntiqueReprodAccbtn = By.xpath("(//div[contains(@class,'imc-filter imc-content imc-expand-collapse')]//div[1]//div[1]//div[2]//div[1]//div[20])[1]"); //Locator for Antique Reprod Accbtn
+	By atlexhibitor = By.xpath("//div[@class='imc-vr--xxlarge']/div[1]/div[1]/div[1]/div[1]//a[1]/h2[1]");//Locator for 1st Exhibitor
+	By stylesFilterbtn = By.xpath("//a[@aria-label='Styles']"); //Locator for Styles filter
 	
-
 	public ATLLeftPaneFilters(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		this.driver = driver; 			
@@ -29,7 +31,22 @@ public class ATLLeftPaneFilters {
 		wait.until(ExpectedConditions.visibilityOfElementLocated(accentfurnexpandbtn));
 		return driver.findElement(accentfurnexpandbtn);
 	}
+	public WebElement getATLAntiqueReprodAccbtn(){
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(atlAntiqueReprodAccbtn));
+		return driver.findElement(atlAntiqueReprodAccbtn);
+	}
 
+	public WebElement getATLexhibitor(){
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(atlexhibitor));
+		return driver.findElement(atlexhibitor);
+	}
+	public WebElement getATLStylesFilterbtn(){
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(stylesFilterbtn));
+		return driver.findElement(stylesFilterbtn);
+	}
 }
 
 
