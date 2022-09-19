9a3d7c7026959268a4f15041baa2f72bde58a49b
Nilam Mahajan <nilamma@cybage.com>
2022-09-19 13:11:05 +0000

    Updated GlobalSearch_SearchFor.java

--- src/test/java/atlantamarket_PROD/GlobalSearch_SearchFor.java
+++ src/test/java/atlantamarket_PROD/GlobalSearch_SearchFor.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
+//import org.omg.CORBA.PUBLIC_MEMBER;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.StaleElementReferenceException;
@@ -24,7 +24,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
+//import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
 
 import pageObjects.AtlantaMarket.ATLExhDigiShowroomPage;
 import pageObjects.AtlantaMarket.ATLExhLineProdActionsPage;
