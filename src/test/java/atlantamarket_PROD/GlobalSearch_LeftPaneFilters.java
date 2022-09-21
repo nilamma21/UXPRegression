55deaecb0f515bcf204fef8df47e347262fb8374
Nilam Mahajan <nilamma@cybage.com>
2022-09-21 06:34:52 +0000

    Updated GlobalSearch_LeftPaneFilters.java

--- src/test/java/atlantamarket_PROD/GlobalSearch_LeftPaneFilters.java
+++ src/test/java/atlantamarket_PROD/GlobalSearch_LeftPaneFilters.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
