d721eed6a29541e9275ca770f8e118a9b95b00d2
SwapnilIrnak91” <swapnili@cybage.com>
2022-11-07 19:38:44 +0530

    LineDigiShowroomFullRun

--- src/main/java/pageObjects/AtlantaMarket/ATLLineDigitalShowroomPage.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLLineDigitalShowroomPage.java
@@ -1,5 +1,7 @@
 package pageObjects.AtlantaMarket;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,14 +20,88 @@ public class ATLLineDigitalShowroomPage {
 	By addToListIcon = By.xpath("//div[@class='imc-section--full-width-mobile imc-content--display-flex imc-content--display-flex-grow-1']/div[1]/div[1]/div[4]/ul[1]/li[2]/button[1]"); //Locator for Add to list
 	By atl1stListName = By.xpath("(//div[@class='imc-vr--xlarge'])[2]/div[1]/label[1]"); //Locator for 1st List name
 	By addNoteIcon = By.xpath("//div[@class='imc-section--full-width-mobile imc-content--display-flex imc-content--display-flex-grow-1']/div[1]/div[1]/div[4]/ul[1]/li[3]/div[1]/div[1]/div[1]/button[1]"); //Locator for Add Note
-	By productcategsectn = By.xpath("//h5[contains(text(),'Product Categories')]"); //Locator for Products Categories section title
-	By productcategitemlist = By.xpath("//div[contains(@class,'imc-gallery imc-gallery--justify-left imc-gallery')]"); //Locator for Product Categories list
-	By prodcatgtable = By.xpath("//div[@class='imc-gallery__item']"); //Locator for Product Categories table
-
+	By orderOnJuniperBtn = By.xpath("//div[@class='imc-section imc-padding--left--xlarge imc-padding--right--xlarge imc-section--width-100-percent']/div[1]/div[2]/a[1]"); //Locator for Order on Juniper Btn
+	By seeAllProductBtn = By.xpath("//div[@class='imc-section imc-padding--left--xlarge imc-padding--right--xlarge imc-section--width-100-percent']/div[1]/div[3]/a[1]/span[1]"); //Locator for See All Product Count Btn
+	By productTitle = By.xpath("//div[@class='imc-section imc-section--width-100-percent']/div[1]/span[1]"); //Locator for Product  titele
+	By productCount = By.xpath("//div[@class='imc-section imc-section--width-100-percent']/div[1]/span[2]"); //Locator for Product Count
+	By productTitleAndCount = By.xpath("//div[@class='imc-section imc-section--width-100-percent']/div[1]/span[contains(text(),'Products Shown')]/../span[1]"); //Locator for Product  titele and count
+	By listOfproductTile = By.xpath("//div[@class='imc-catalog__item_image-wrapper']"); //Locator for Lists of Product  tiles 
+	By orderOnJuniperBtnLink = By.xpath("//div[@class='imc-products-overview--cta-bar__item']/a[1]"); //Locator for Order on Juniper Btn Bottom Link
+	By prodctCountBottomBtn = By.xpath("//div[@class='imc-products-overview--cta-bar__item imc-padding--left--xlarge--desktop']/a[1]/span[1]"); //Locator for Product count Btn Bottom Link
+	By seeAllprodctCountBottomBtn = By.xpath("//div[@class='imc-products-overview--cta-bar__item imc-padding--left--xlarge--desktop']/a[1]"); //Locator for Product count Btn Bottom Link
+	By productName= By.xpath("//div[@class='imc-products-overview--gallery']/div[1]/div[1]/a[1]/div[1]/div[1]/span[1]/span[1]/span[1]"); //Locator for Product name
+	By productHeader= By.xpath("//h2[@class='imc-padding--left--smallmedium imc-padding--bottom--small']"); //Locator for Product name
+	By productName1= By.xpath("//div[@class='imc-products-overview--gallery']/div[1]/div[1]/a[1]/div[1]/div[1]/span[1]/span[1]"); //Locator for Product name
+	
+	
+	
 	public ATLLineDigitalShowroomPage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
 		this.driver = driver; 			
-	} 
+	}
+
+	
+	public WebElement getproductName1() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productName1));
+		return driver.findElement(productName1);
+	}
+	public WebElement getproductHeader() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productHeader));
+		return driver.findElement(productHeader);
+	}
+	public WebElement getproductName() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productName));
+		return driver.findElement(productName);
+	}
+	public WebElement getSeeAllprodctCountBottomBtn() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(seeAllprodctCountBottomBtn));
+		return driver.findElement(seeAllprodctCountBottomBtn);
+	}
+	public WebElement getprodctCountBottomBtn() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(prodctCountBottomBtn));
+		return driver.findElement(prodctCountBottomBtn);
+	}
+	public WebElement getorderOnJuniperBtnLink() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(orderOnJuniperBtnLink));
+		return driver.findElement(orderOnJuniperBtnLink);
+	}
+
+	public List <WebElement> gelistOfproductTile() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(listOfproductTile));
+		return driver.findElements(listOfproductTile);
+	}
+	public WebElement getproductTitleAndCount() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productTitleAndCount));
+		return driver.findElement(productTitleAndCount);
+	}
+	public WebElement getproductCount() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productCount));
+		return driver.findElement(productCount);
+	}
+	public WebElement getproductTitle() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(productTitle));
+		return driver.findElement(productTitle);
+	}
+	public WebElement getseeAllProductBtn() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(seeAllProductBtn));
+		return driver.findElement(seeAllProductBtn);
+	}
+	public WebElement getorderOnJuniperBtn() {
+		wait = new WebDriverWait (driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(orderOnJuniperBtn));
+		return driver.findElement(orderOnJuniperBtn);
+	}
 	public WebElement getaddNoteIcon() {
 		wait = new WebDriverWait (driver,20);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(addNoteIcon));
