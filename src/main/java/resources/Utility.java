cce8cf2b832269e071776748b71f4b4d6be396b8
Nilam Mahajan <nilamma@cybage.com>
2022-12-05 12:33:32 +0000

    Removed cobra pckg frm Utility.java

--- src/main/java/resources/Utility.java
+++ src/main/java/resources/Utility.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
-import org.omg.CORBA.PUBLIC_MEMBER;
+//import org.omg.CORBA.PUBLIC_MEMBER;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
