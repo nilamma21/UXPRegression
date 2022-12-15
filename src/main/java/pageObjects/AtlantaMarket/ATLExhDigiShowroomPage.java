cad4ddfbfd92f1285978790df25f92bca3909300
Nilam Mahajan <nilamma@cybage.com>
2022-12-15 07:56:51 +0000

    Updated ATLExhDigiShowroomPage.java

--- src/main/java/pageObjects/AtlantaMarket/ATLExhDigiShowroomPage.java
+++ src/main/java/pageObjects/AtlantaMarket/ATLExhDigiShowroomPage.java
@@ -13,7 +13,7 @@ public class ATLExhDigiShowroomPage {
 	public WebDriver driver;
 	public WebDriverWait wait;
 
-	By atlvalidateexhdigishowpage = By.xpath("//img[@class='imc-exhibitors--detail-image']"); // Locator to validate Exhibitor Digital Showroom page
+	By atlvalidateexhdigishowpage = By.xpath("(//img[contains(@class,'imc-exhibitors--')])[2]"); // Locator to validate Exhibitor Digital Showroom page
 	By lvmvalidateexhdigishowpage_uat = By.xpath("//div[@class='imc-gallery__item']"); // Locator to validate Exhibitor Digital Showroom page
 	By atlvalidateexhdigishowpage_lvmUAT = By.xpath("//img[@class='imc-exhibitors--detail-image']"); // Locator to validate Exhibitor Digital Showroom page LVM UAT
 	By exhibitornameonexhdirectimg = By.xpath("//span[@class='imc-heading imc-heading--giga-desktop ']"); //Locator for Exhibitor name on Exhibitor Directory image
