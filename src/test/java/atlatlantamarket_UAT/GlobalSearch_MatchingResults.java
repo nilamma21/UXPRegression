9800fa8fd47662ccb3cce41285af83565be3e76d
Swapnil Irnak <swapnili@cybage.com>
2023-01-12 09:27:15 +0000

    Updated GlobalSearch_MatchingResults.java

--- src/test/java/atlatlantamarket_UAT/GlobalSearch_MatchingResults.java
+++ src/test/java/atlatlantamarket_UAT/GlobalSearch_MatchingResults.java
@@ -55,7 +55,7 @@ public class GlobalSearch_MatchingResults extends base {
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("atlmrkturl_uat"));
 
-		lap.getIUnderstandBtn().click();
+		//lap.getIUnderstandBtn().click();
 		Thread.sleep(5000);
 		utl.CloseATLPopup();
 
