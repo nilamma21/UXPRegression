771f8b2996c9cb4fa9ff3ec0a55d19ba05064104
Swapnil Irnak <swapnili@cybage.com>
2023-01-12 09:28:30 +0000

    Updated ShowSpecials.java

--- src/test/java/atlatlantamarket_UAT/ShowSpecials.java
+++ src/test/java/atlatlantamarket_UAT/ShowSpecials.java
@@ -52,7 +52,7 @@ public class ShowSpecials extends base  {
 		driver.manage().window().maximize();
 		driver.get(prop.getProperty("atlmrkturl_uat"));
 		
-		lap.getIUnderstandBtn().click();
+		//lap.getIUnderstandBtn().click();
 		Thread.sleep(5000);
 		utl.CloseATLPopup();
 		
