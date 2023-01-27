bc0f80e9e12dfa5e08464289b79ee617a90a8665
Nilam Mahajan <nilamma@cybage.com>
2023-01-27 07:46:16 +0000

    Updated GlobalSearch_SearchFor.java

--- src/test/java/atlantamarket_PROD/GlobalSearch_SearchFor.java
+++ src/test/java/atlantamarket_PROD/GlobalSearch_SearchFor.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
@@ -820,5 +821,9 @@ public class GlobalSearch_SearchFor extends base {
 		driver.navigate().back();
 		driver.get(prop.getProperty("atlmrkturl_prod"));
 		Thread.sleep(5000);
+	}
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 }
