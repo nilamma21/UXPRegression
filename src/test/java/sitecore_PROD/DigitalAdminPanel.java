4a08b23ef25a16e1650bcc026e32d6d20179d50f
prasannasheelb <prasannasheelb@cybage.com>
2024-02-07 16:41:13 +0530

    Initial commit: created empty folders for Sitecore scripts

---
+++ src/test/java/sitecore_PROD/DigitalAdminPanel.java
@@ -0,0 +1,19 @@
+package sitecore_PROD;
+
+import org.testng.annotations.Listeners;
+
+import atlantamarket_UAT.TestListeners;
+import resources.base;
+
+/*
+ * 
+ *
+ * NOTE: Create automation tests for SiteCore regression, refer zephyr '/Regression Test_Cyb_New/SiteCore/Digital Admin Panel'
+ * 
+ * 
+*/
+
+
+public class DigitalAdminPanel extends base{
+	
+}
